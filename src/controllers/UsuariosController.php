c7585dcaca4a261917804328a02c7e6f021f7760
Luiz Gustavo <luizgustavop2@outlook.com.br>
2021-01-06 23:34:55 -0300

    Criado model do Usuario, metodo de adicao no controller e seu respectivo form

--- src/controllers/UsuariosController.php
+++ src/controllers/UsuariosController.php
@@ -3,6 +3,7 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\models\Usuario;
 
 class UsuariosController extends Controller
 {
@@ -13,6 +14,24 @@ class UsuariosController extends Controller
 
     public function addAction()
     {
-        echo 'Recebido';
+        $name     = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+        $email    = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+
+        if($name && $email)
+        {
+            $data    = Usuario::select()->where('email', $email)->execute();
+
+            if(count($data) === 0)
+            {
+                Usuario::insert([
+                    'nome' => $name,
+                    'email' => $email
+                ])->execute();
+
+                $this->redirect('/');
+            }
+        }
+        
+        $this->redirect('/novo');
     }
 }
--- src/views/pages/add.php
+++ src/views/pages/add.php
@@ -2,8 +2,15 @@
 
 <h2>Adicionar novo Usuário</h2>
 
-<?php $render('footer'); ?>
-
 <form action="<?=$base;?>/novo" method="post">
+
+    <label for="name">Nome:</label>
+    <input type="text" name="name" id="name" autofocus required>
     
+    <label for="email">E-mail</label>
+    <input type="email" name="email" id="email" required>
+
+    <button type="submit">Adicionar</button>
 </form>
+
+<?php $render('footer'); ?>
